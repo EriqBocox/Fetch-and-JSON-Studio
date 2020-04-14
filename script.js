a98ea9c7d2c2f26679e8f9528510380c20262879
eriq <erbocox@aol.com>
2020-04-14 13:21:27 -0400

    added time in space order and active changes color

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ window.addEventListener('load', function(){
     fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response){
         response.json().then(function(json){
             const container = document.getElementById('container');
+
+
+            json.sort(function(b,a){
+                return(a.hoursInSpace-b.hoursInSpace);
+            });
+
+
             for(index=0; index<json.length; index++){
+                let color = (json[index].active) ? 'green' : 'red';
+
                 container.innerHTML += `
                     <div class="astronaut">
                         <div class="bio">
                             <h3>${json[index].firstName} ${json[index].lastName}</h3>
                             <ul>    
                                 <li>Hours in Space: ${json[index].hoursInSpace}</li>
-                                <li>Active: ${json[index].active}</li>
+                                <li style="color:${color}">Active: ${json[index].active}</li>
                                 <li>Skills: ${json[index].skills}</li>
                             </ul>
                         </div>
